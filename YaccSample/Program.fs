c96095ae3f5e95982613bec1d2303249baa40cc2
Obeleh <sjuulj@hotmail.com>
2011-07-07 09:13:55 +0200

    First Commit

---
+++ YaccSample/Program.fs
@@ -0,0 +1,18 @@
+﻿open System   
+open Sql   
+ 
+let x = "   
+    SELECT x, y, z   
+    FROM t1   
+    LEFT JOIN t2   
+    INNER JOIN t3 ON t3.ID = t2.ID   
+    WHERE x = 50 AND y = 20   
+    ORDER BY x ASC, y DESC, z   
+"   
+ 
+let lexbuf = Lexing.LexBuffer<_>.FromString x
+let y = SqlParser.start SqlLexer.tokenize lexbuf   
+printfn "%A" y   
+ 
+Console.WriteLine("(press any key)")   
+Console.ReadKey(true) |> ignore
